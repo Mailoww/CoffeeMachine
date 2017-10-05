45ea4fa443793fa6392102520f8150fc7f5be489
MelodyBibi <melody.scholtes@gmail.com>
2017-10-05 11:47:20 +0200

    Renamming test with_this_convention

--- CoffeeMachine/src/test/java/DrinkManagerTest.java
+++ CoffeeMachine/src/test/java/DrinkManagerTest.java
@@ -4,67 +4,67 @@ import org.junit.Test;
 public class DrinkManagerTest {
 
     @Test
-    public void shouldMakeCoffee(){
+    public void should_make_coffee(){
         Assert.assertEquals("C::", new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_COFFEE)));
     }
 
     @Test
-    public void shouldMakeTea(){
+    public void should_make_tea(){
         Assert.assertEquals("T::", new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_TEA)
         ));
     }
 
     @Test
-    public void shouldMakeChocolate(){
+    public void should_make_chocolate(){
         Assert.assertEquals("H::", new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_CHOCOLATE)
         ));
     }
 
     @Test
-    public void shouldMakeCoffeeWithOneSugar(){
+    public void should_make_coffee_with_one_sugar(){
         Assert.assertEquals("C:1:0", new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_COFFEE, 1)
         ));
     }
 
     @Test
-    public void shouldMakeTeaWithOneSugar(){
+    public void should_make_tea_with_one_sugar(){
         Assert.assertEquals("T:1:0", new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_TEA, 1)
         ));
     }
 
     @Test
-    public void shouldMakeChocolateWithOneSugar(){
+    public void should_make_chocolate_with_one_sugar(){
         Assert.assertEquals("H:1:0", new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_CHOCOLATE, 1)
         ));
     }
 
     @Test
-    public void shouldMakeDrinkWithTwoSugar(){
+    public void should_make_drink_with_two_sugar(){
         Assert.assertEquals("H:2:0", new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_CHOCOLATE, 2)
         ));
     }
 
     @Test(expected=IllegalArgumentException.class)
-    public void shouldThrowExceptionMakeDrinkWithThreeSugar(){
+    public void should_throw_exception_when_make_drink_with_three_sugar(){
         new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_CHOCOLATE, 3));
     }
 
     @Test(expected=IllegalArgumentException.class)
-    public void shouldThrowExceptionMakeDrinkWithNegativeNumberSugar(){
+    public void should_throw_exception_when_make_drink_with_negative_number_sugar(){
         new DrinkManager().make(
                 new Drink(DrinkManager.CHARACTER_FOR_CHOCOLATE, -1));
     }
 
     @Test
-    public void shouldSendAMessage(){
+    public void should_send_a_message(){
         String message = "Message test";
         Assert.assertEquals("M:" + message, new DrinkManager().send(message));
     }
