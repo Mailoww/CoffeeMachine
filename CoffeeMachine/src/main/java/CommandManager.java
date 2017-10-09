dbe435d24f23b71412bd687aab620bad8f84e489
MelodyBibi <melody.scholtes@gmail.com>
2017-10-09 17:14:29 +0200

    Refactoring methode produce pour avoir moins de paramètres

--- CoffeeMachine/src/main/java/CommandManager.java
+++ CoffeeMachine/src/main/java/CommandManager.java
@@ -6,21 +6,25 @@ public class CommandManager {
     private static final double PRICE_COFFEE = 0.6;
     private static final double PRICE_CHOCOLATE = 0.5;
     private static final double PRICE_TEA = 0.4;
+    private Drink drink;
+    private double currentPrice;
 
     public static CommandManager create() {
         return new CommandManager();
     }
 
     public String make(Drink drink, double price) {
+        this.drink = drink;
+        this.currentPrice = price;
         if(drink.isCoffee())
-            return produce(drink, price, PRICE_COFFEE);
+            return produce(PRICE_COFFEE);
         else if(drink.isTea())
-            return produce(drink, price, PRICE_TEA);
+            return produce(PRICE_TEA);
         else
-            return produce(drink, price, PRICE_CHOCOLATE);
+            return produce(PRICE_CHOCOLATE);
     }
 
-    private String produce(Drink drink, double currentPrice, double expectedPrice) {
+    private String produce(double expectedPrice) {
         if(currentPrice == expectedPrice)
             return DrinkManager.make(drink);
         else
